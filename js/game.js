e264959568f4870dceb8eebacdef1455e16ddb53
ngeudapdjoumessi-coder <ngeudapdjoumessi@gmail.com>
2026-07-29 05:48:01 +0100

    Create js/game.js

---
+++ js/game.js
@@ -0,0 +1,60 @@
+/*
+=========================================
+GALAXY NOVA
+GAME.JS
+=========================================
+*/
+
+"use strict";
+
+// Joueur
+let player = null;
+
+// Démarrage
+document.getElementById("playBtn").addEventListener("click", () => {
+
+    document.getElementById("mainMenu").classList.add("hidden");
+
+    document.getElementById("hud").classList.remove("hidden");
+
+    startNewGame();
+
+});
+
+// Nouvelle partie
+function startNewGame(){
+
+    player = new Player();
+
+    GAME.level = 1;
+    GAME.score = 0;
+    GAME.coins = CONFIG.START_COINS;
+    GAME.lives = CONFIG.START_LIVES;
+
+    startGame();
+
+}
+
+// Mise à jour
+
+function updateGame(delta){
+
+    if(player){
+
+        player.update(delta);
+
+    }
+
+}
+
+// Dessin
+
+function renderGame(){
+
+    if(player){
+
+        player.draw();
+
+    }
+
+}
